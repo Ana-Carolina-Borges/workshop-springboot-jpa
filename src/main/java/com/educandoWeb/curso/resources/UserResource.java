985aa5fdda92c71b50d85db694fa0a8216d308ca
Ana Carolina <carolbd1107@gmail.com>
2022-12-13 19:31:24 -0300

    User entity and resource

---
+++ src/main/java/com/educandoWeb/curso/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandoWeb.curso.resources;
+
+
+import com.educandoWeb.curso.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Carol", "carol@gamil.com", "99250862", "1107");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
