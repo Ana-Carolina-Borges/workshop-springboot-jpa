335ec5f67796af4be42f2359564824418058efc7
Ana Carolina <carolbd1107@gmail.com>
2022-12-14 09:31:26 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoWeb/curso/entities/User.java
+++ src/main/java/com/educandoWeb/curso/entities/User.java
@@ -1,12 +1,19 @@
 package com.educandoWeb.curso.entities;
 
 
+import jakarta.persistence.*;
+import org.springframework.data.repository.cdi.Eager;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
