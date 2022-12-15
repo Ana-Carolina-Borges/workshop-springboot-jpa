959a54d4a4babe5ae90b4ff6453e4f841e72a202
Ana Carolina <carolbd1107@gmail.com>
2022-12-15 12:02:22 -0300

    Category

---
+++ src/main/java/com/educandoWeb/curso/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.educandoWeb.curso.repositories;
+
+import com.educandoWeb.curso.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
